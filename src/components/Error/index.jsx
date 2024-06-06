9a364f59df131d21926e6709b67703cd7dc7feec
Mégane Navarro <mnavarro@live.fr>
2024-06-06 13:31:26 +0200

    Création de la page d'erreur

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,7 +1,9 @@
 function Error() {
     return (
-        <div>
-            <h1>Oups 🙈 Cette page n'existe pas</h1>
+        <div className="container-error">
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <a href="/">Retourner sur la page d'accueil</a>
         </div>
     )
 }
