ac1514ddf002da931d140a2ee4bbaace36e0cb41
Mégane Navarro <mnavarro@live.fr>
2024-06-13 09:44:45 +0200

    Mise en place du slideshow et corrections CSS

---
+++ src/components/Slideshow/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import arrowLeft from '../../images/arrow_left.svg';
+import arrowRight from '../../images/arrow_right.svg';
+
+function Slideshow(props) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const slides = props.pictures
+    const previous = () => {
+        let newIndex
+        if(currentIndex === 0) {
+            newIndex = slides.length -1
+        }
+        else {
+            newIndex = currentIndex -1
+        }
+        setCurrentIndex(newIndex);
+    }
+
+    const next = () => {
+        let newIndex
+        if(currentIndex === slides.length -1) {
+            newIndex = 0
+        }
+        else {
+            newIndex = currentIndex +1
+        }
+        setCurrentIndex(newIndex);
+    }
+
+    return (
+        <section className="slideshow">
+            <img src={slides[currentIndex]} alt={props.alt} className="slide"></img>
+                <div className="directions">
+                    <img src={arrowLeft} alt="Flèche navigation gauche" className="arrow-left" onClick={previous}></img>
+                    <img src={arrowRight} alt="Flèche navigation droite" className="arrow-right" onClick={next}></img>
+                </div>
+                <div className="pagination">{currentIndex+1} / {slides.length}</div>
+        </section>
+    )
+}
+export default Slideshow
--- src/pages/Accommodation/index.jsx
+++ src/pages/Accommodation/index.jsx
@@ -1,16 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import accommodations from '../../data/logements.json';
 import Collapse from '../../components/Collapse'
 import Rating from '../../components/Rating'
+import Slideshow from '../../components/Slideshow'
 
 function Accommodation() {
     const { id } = useParams();
     const accommodationData = accommodations.filter(element =>
         element.id === id
     )
+    if(accommodationData.length === 0) {
+        return <Navigate to="/404" />
+    }
     const accommodation = accommodationData.map(data =>
         <div key={data.id}>
-            <img src={data.cover} alt={data.title} className="cover"></img>
+            <Slideshow cover={data.cover} alt={data.title} pictures={data.pictures} />
             <section className="accommodation-informations">
                 <div className="accommodation-general">
                     <h1>{data.title}</h1>
